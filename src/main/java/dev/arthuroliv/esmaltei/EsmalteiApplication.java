0c59b35631bed992b30fb01551a01b3ef36bb21e
Thurrrrr <arthur.ant.oliv@gmail.com>
2026-07-13 14:50:56 -0300

    chore: cria estrutura inicial do projeto Spring Boot

---
+++ src/main/java/dev/arthuroliv/esmaltei/EsmalteiApplication.java
@@ -0,0 +1,13 @@
+package dev.arthuroliv.esmaltei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsmalteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsmalteiApplication.class, args);
+	}
+
+}
